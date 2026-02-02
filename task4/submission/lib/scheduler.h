54600e4e88fe87060bb6dd8725a7d3f0f6855473
srirams2204 <sriram.s0011@gmail.com>
2026-02-02 23:17:20 +0530

    added task4 to repo

--- task4/submission/lib/scheduler.h
+++ task4/submission/lib/scheduler.h
@@ -3,20 +3,17 @@
 
 #include <stdint.h>
 
-// Define a function pointer type for tasks
 typedef void (*TaskFunction_t)(void);
 
-// Task Control Block (TCB)
 typedef struct {
-    TaskFunction_t taskFunc;  // Pointer to the task function
-    uint32_t period_ms;       // How often to run (in ms)
-    uint32_t last_run;        // Last time it ran (system ticks)
+    TaskFunction_t taskFunc;  
+    uint32_t period_ms;       
+    uint32_t last_run;        
 } Task_t;
 
-// API Functions
 void OS_Init(void);
 void OS_AddTask(TaskFunction_t func, uint32_t period);
-void OS_Run(void); // The "kernel" loop
-uint32_t OS_GetTick(void); // Returns current system time
+void OS_Run(void); 
+uint32_t OS_GetTick(void); 
 
 #endif
